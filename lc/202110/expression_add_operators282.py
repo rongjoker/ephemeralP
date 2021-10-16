77ff6d806bd3f70b7efee874dcc2653db0c890dc
zhangshipeng <magic_of_java@foxmail.com>
2021-10-16 14:18:33 +0800

    backtrack

--- lc/202110/expression_add_operators282.py
+++ lc/202110/expression_add_operators282.py
@@ -10,43 +10,44 @@ class Solution:
         nums = []
         for n in num:
             nums.append(ord(n) - ord('0'))
-        print(nums)
         self.sum_val += nums[0]
         self.str_code.append(str(nums[0]))
-        self.backtrack(nums, 1, target, len(nums))
-        return self.ans
 
-    def backtrack(self, nums: List[int], index: int, target: int, length: int):
-        if index == length:
-            print('self.sum_val', self.sum_val)
-            print('self.str_code', self.str_code)
-            if self.sum_val == target:
-                self.ans.append(''.join(self.str_code))
-            return
-        size = len(self.str_code)
-        self.sum_val += nums[index]
-        self.str_code.append('+')
-        self.str_code.append(str(nums[index]))
-        self.backtrack(nums, index + 1, target, length)
-        # back
-        del self.str_code[size:]
-        self.sum_val -= nums[index]
-        # track
-        self.sum_val *= nums[index]
-        self.str_code.append('*')
-        self.str_code.append(str(nums[index]))
-        self.backtrack(nums, index + 1, target, length)
-        # back
-        del self.str_code[size:]
-        self.sum_val //= nums[index]
-        # track
-        self.sum_val -= nums[index]
-        self.str_code.append('-')
-        self.str_code.append(str(nums[index]))
-        self.backtrack(nums, index + 1, target, length)
-        # back
-        del self.str_code[size:]
-        self.sum_val += nums[index]
+        def backtrack(nums: List[int], index: int, target: int, length: int):
+            if index == length:
+                print('self.sum_val', self.sum_val)
+                print('self.str_code', self.str_code)
+                if self.sum_val == target:
+                    self.ans.append(''.join(self.str_code))
+                return
+            size = len(self.str_code)
+            self.sum_val += nums[index]
+            self.str_code.append('+')
+            self.str_code.append(str(nums[index]))
+            backtrack(nums, index + 1, target, length)
+            # back
+            del self.str_code[size:]
+            self.sum_val -= nums[index]
+            # track
+            self.sum_val *= nums[index]
+            self.str_code.append('*')
+            self.str_code.append(str(nums[index]))
+            backtrack(nums, index + 1, target, length)
+            # back
+            del self.str_code[size:]
+            self.sum_val //= nums[index]
+            # track
+            self.sum_val -= nums[index]
+            self.str_code.append('-')
+            self.str_code.append(str(nums[index]))
+            backtrack(nums, index + 1, target, length)
+            # back
+            del self.str_code[size:]
+            self.sum_val += nums[index]
+            
+        backtrack(nums, 1, target, len(nums))
+
+        return self.ans
 
 
 a = Solution()
